bdd33de8ed28e94264f7a9d23e5d9f31154febb0
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2021-04-12 15:56:31 +0400

    Добавил папку для моделей и добавил в нее 2 модели.

---
+++ Diploma/Models/UserData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma.Models
+{
+    class UserData
+    {
+        public long recordId { get; set; }
+        public long userId { get; set; }
+        public virtual User user { get; set; }
+        public string path { get; set; }
+        public string name { get; set; }
+    }
+}
