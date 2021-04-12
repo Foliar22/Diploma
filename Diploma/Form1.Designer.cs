16771201c4c086da9f7d59a4756ac40eaebce323
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2021-04-12 19:33:36 +0400

    Сделал datagridview и настроил его визуальный стиль и его заполнение данными.

--- Diploma/Form1.Designer.cs
+++ Diploma/Form1.Designer.cs
@@ -29,20 +29,81 @@ namespace Diploma
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.Controls.Add(this.dataGridViewMain);
+            this.panelMain.Location = new System.Drawing.Point(0, 46);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(800, 373);
+            this.panelMain.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(69, 13);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(130, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMain.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewMain.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewMain.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewMain.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewMain.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.dataGridViewMain.Size = new System.Drawing.Size(800, 373);
+            this.dataGridViewMain.TabIndex = 0;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.panelMain);
             this.Name = "FormMain";
             this.Text = "DW";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DataGridView dataGridViewMain;
     }
 }
 
--- Diploma/Form1.cs
+++ Diploma/Form1.cs
@@ -7,14 +7,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security;
+using Diploma.DAL;
+using Diploma.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Diploma
 {
     public partial class FormMain : Form
     {
+        private SqlCommandBuilder sqlBuilder { get; set; }
+        private SqlDataAdapter sqlDataAdapter { get; set; }
+        private DataSet dataSet { get; set; }
+        private string connectionString { get; set; }
         public FormMain()
         {
             InitializeComponent();
+           
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.ShowNewFolderButton = false;
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(FBD.SelectedPath);
+                var likeExe = "*.exe";
+                var path = FBD.SelectedPath;
+                try
+                {
+                    foreach (var findedFile in Directory.EnumerateFiles(path, likeExe, SearchOption.AllDirectories))
+                    {
+                        FileInfo fileInfo = new FileInfo(findedFile);
+                        MessageBox.Show($"Имя {fileInfo.Name}\npath {fileInfo.FullName}");
+                        using (DataContext context = new DataContext())
+                        {
+                            var user = context.users.Where(u => u.userId == 1).FirstOrDefault();
+                            UserData userData = new UserData()
+                            {
+                                userId = 1,
+                                name = fileInfo.Name,
+                                path = fileInfo.FullName,
+                                user = user
+
+                            };
+                            context.userDatas.Add(userData);
+                            context.SaveChanges();
+                        }
+
+                    }
+                }
+                catch (SecurityException)
+                {
+                    MessageBox.Show("Доступ запрещен");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Доступ запрещен");
+                }
+
+            }
+        }
+
+        private void ShowData(SqlConnection sqlConnection)
+        {
+            sqlDataAdapter = new SqlDataAdapter("Select name as [Название],'Удалить' as [Удалить] from userDatas where userId = 1", sqlConnection);
+            sqlBuilder = new SqlCommandBuilder(sqlDataAdapter);
+            dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet, "UserData");
+            dataGridViewMain.DataSource = dataSet.Tables["UserData"];
+            for(int i = 0; i < dataGridViewMain.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridViewMain.ColumnCount; j++)
+                {
+                    dataGridViewMain.Rows[i].Cells[j].ReadOnly = true;
+                }
+            }
+        
+            for (int i = 0; i < dataGridViewMain.Rows.Count; i++)
+            {
+                DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                linkCell.LinkColor = Color.Black; // Устанавливает черный цвет для текста
+                dataGridViewMain[1, i] = linkCell;
+            }
+            for(int i = 0; i < dataGridViewMain.Rows.Count; i++)
+            {
+                DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                linkCell.LinkColor = Color.Black;
+                dataGridViewMain[0, i] = linkCell;
+                dataGridViewMain[0,i].Style.Alignment = DataGridViewContentAlignment.MiddleLeft; // Устанавливает текст слева, а для другого столбца в конструкторе установленно на центр
+            }
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            dataGridViewMain.RowTemplate.Height = 40;
+            dataGridViewMain.RowHeadersVisible = false; // Удаляет леввый столбец в отображении таблиц
+            dataGridViewMain.DefaultCellStyle.SelectionBackColor = Color.White; // Устанавливает цвет выбранной ячейки
+            dataGridViewMain.AllowUserToResizeColumns = false; // Запрещает изменение размера стобца
+            dataGridViewMain.AllowUserToResizeRows = false; // Запрещает изменение размера строки
+            dataGridViewMain.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 14);
+            connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            var sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            ShowData(sqlConnection);
         }
     }
 }
