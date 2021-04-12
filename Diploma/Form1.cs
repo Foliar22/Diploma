44e947992c058b90821b352085c4f4569d748042
Foliar22 <77840299+Foliar22@users.noreply.github.com>
2021-04-12 15:33:05 +0400

    Измененил имя формы и имя разработки формы

--- Diploma/Form1.cs
+++ Diploma/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Diploma
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
